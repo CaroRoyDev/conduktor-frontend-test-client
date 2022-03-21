f3f39cd7334fee491dcbfb5c2ebf060643211ebf
Caroline Roy <caroline.roy.dev@gmail.com>
2022-03-21 01:51:02 +0100

    Moved sider from layout to Nav component

--- src/layouts/app.layout.tsx
+++ src/layouts/app.layout.tsx
@@ -1,28 +1,15 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Layout, Menu, Button } from "antd";
+import { Layout } from "antd";
 import "antd/dist/antd.css";
-import { DoubleRightOutlined, DoubleLeftOutlined } from "@ant-design/icons";
-import {
-  FaHome,
-  FaCogs,
-  FaListAlt,
-  FaOutdent,
-  FaBook,
-  FaStream,
-  FaDatabase,
-  FaLock,
-  FaChartLine,
-} from "react-icons/fa";
-import { GrConnect } from "react-icons/gr";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import Header from "./partials/header";
+import Nav from "./partials/nav";
 import Footer from "./partials/footer";
 import { TranslationContent as T } from "../context/multilingual.context";
 import styles from "./app.module.scss";
 
-const { Content, Sider } = Layout;
+const { Content } = Layout;
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -30,7 +17,7 @@ interface AppLayoutProps {
 
 const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const onCollapseButtonClick = () => {
+  const onCollapse = () => {
     setIsCollapsed(!isCollapsed);
   };
   const { isAuthenticated } = useAuth0();
@@ -41,52 +28,7 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
       <Layout className={styles["content-layout"]}>
         {isAuthenticated ? (
           <>
-            <Sider
-              collapsible
-              collapsed={isCollapsed}
-              trigger={null}
-              className={styles.sider}
-            >
-              <Menu defaultSelectedKeys={["1"]} mode="inline">
-                <Menu.Item key="1" icon={<FaHome />}>
-                  <Link to="/overview">Overview</Link>
-                </Menu.Item>
-                <Menu.Item key="2" icon={<FaCogs />}>
-                  <Link to="/Brokers">Brokers</Link>
-                </Menu.Item>
-                <Menu.Item key="3" icon={<FaListAlt />}>
-                  <Link to="/topics">Topics</Link>
-                </Menu.Item>
-                <Menu.Item key="4" icon={<FaOutdent />}>
-                  <Link to="/consumers">Consumers</Link>
-                </Menu.Item>
-                <Menu.Item key="5" icon={<FaBook />}>
-                  <Link to="/schema-registry">Schema Registry</Link>
-                </Menu.Item>
-                <Menu.Item key="6" icon={<GrConnect />}>
-                  <Link to="/kafka-connect">Kafka Connect</Link>
-                </Menu.Item>
-                <Menu.Item key="7" icon={<FaStream />}>
-                  <Link to="/kafka-connect">Kafka Streams</Link>
-                </Menu.Item>
-                <Menu.Item key="8" icon={<FaDatabase />}>
-                  <Link to="/ksqlDB">ksqlDB</Link>
-                </Menu.Item>
-                <Menu.Item key="9" icon={<FaLock />}>
-                  <Link to="/security">Security</Link>
-                </Menu.Item>
-                <Menu.Item key="10" icon={<FaChartLine />}>
-                  <Link to="/performance">Performance</Link>
-                </Menu.Item>
-              </Menu>
-              <Button
-                className={styles["collapse-button"]}
-                onClick={onCollapseButtonClick}
-                type="default"
-              >
-                {isCollapsed ? <DoubleRightOutlined /> : <DoubleLeftOutlined />}
-              </Button>
-            </Sider>
+            <Nav isCollapsed={isCollapsed} onCollapse={onCollapse} />
             <Content>{children}</Content>
           </>
         ) : (
--- src/layouts/partials/nav.tsx
+++ src/layouts/partials/nav.tsx
@@ -1,9 +1,77 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Layout, Menu, Button } from "antd";
+import { DoubleRightOutlined, DoubleLeftOutlined } from "@ant-design/icons";
+import {
+  FaHome,
+  FaCogs,
+  FaListAlt,
+  FaOutdent,
+  FaBook,
+  FaStream,
+  FaDatabase,
+  FaLock,
+  FaChartLine,
+} from "react-icons/fa";
+import { GrConnect } from "react-icons/gr";
+import styles from "./../app.module.scss";
 
-type Props = {};
+const { Sider } = Layout;
 
-const Nav: React.FC = (props: Props) => {
-  return <div>Nav</div>;
+interface NavProps {
+  isCollapsed: boolean;
+  onCollapse: () => void;
+}
+
+const Nav: React.FC<NavProps> = ({ isCollapsed, onCollapse }) => {
+  return (
+    <Sider
+      collapsible
+      collapsed={isCollapsed}
+      trigger={null}
+      className={styles.sider}
+    >
+      <Menu defaultSelectedKeys={["1"]} mode="inline">
+        <Menu.Item key="1" icon={<FaHome />}>
+          <Link to="/overview">Overview</Link>
+        </Menu.Item>
+        <Menu.Item key="2" icon={<FaCogs />}>
+          <Link to="/Brokers">Brokers</Link>
+        </Menu.Item>
+        <Menu.Item key="3" icon={<FaListAlt />}>
+          <Link to="/topics">Topics</Link>
+        </Menu.Item>
+        <Menu.Item key="4" icon={<FaOutdent />}>
+          <Link to="/consumers">Consumers</Link>
+        </Menu.Item>
+        <Menu.Item key="5" icon={<FaBook />}>
+          <Link to="/schema-registry">Schema Registry</Link>
+        </Menu.Item>
+        <Menu.Item key="6" icon={<GrConnect />}>
+          <Link to="/kafka-connect">Kafka Connect</Link>
+        </Menu.Item>
+        <Menu.Item key="7" icon={<FaStream />}>
+          <Link to="/kafka-connect">Kafka Streams</Link>
+        </Menu.Item>
+        <Menu.Item key="8" icon={<FaDatabase />}>
+          <Link to="/ksqlDB">ksqlDB</Link>
+        </Menu.Item>
+        <Menu.Item key="9" icon={<FaLock />}>
+          <Link to="/security">Security</Link>
+        </Menu.Item>
+        <Menu.Item key="10" icon={<FaChartLine />}>
+          <Link to="/performance">Performance</Link>
+        </Menu.Item>
+      </Menu>
+      <Button
+        className={styles["collapse-button"]}
+        onClick={onCollapse}
+        type="default"
+      >
+        {isCollapsed ? <DoubleRightOutlined /> : <DoubleLeftOutlined />}
+      </Button>
+    </Sider>
+  );
 };
 
 export default Nav;
